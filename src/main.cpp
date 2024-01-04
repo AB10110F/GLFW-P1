84aed38688382d685e6b3a7426120c8e959598aa
A. Bryant <W.Sword.09@hotmail.com>
2024-01-04 09:50:52 -0600

    siw

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,112 @@
+// #include <glad/glad.h>
+// #include <GLFW/glfw3.h>
+//
+// #include <iostream>
+//
+// void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+// {
+//     // make sure the viewport matches the new window dimensions; note that width and
+//     // height will be significantly larger than specified on retina displays.
+//     glViewport(0, 0, width, height);
+// }
+//
+// void processInput(GLFWwindow *window)
+// {
+//     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+//         glfwSetWindowShouldClose(window, true);
+// }
+//
+// int main() {
+//     glfwInit();
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//
+//     float vertices[] = {
+//     -0.5f, -0.5f, 0.0f,
+//      0.5f, -0.5f, 0.0f,
+//      0.0f,  0.5f, 0.0f
+//     };
+//
+//     unsigned int VBO;  //Vertex Buffer Object
+//     glGenBuffers(1, &VBO);
+//     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//
+//     GLFWwindow* window = glfwCreateWindow(1200, 600, "WINDOW NAME", NULL, NULL);
+//     if (window == NULL)
+//     {
+//         std::cout << "Failed to create GLFW window" << std::endl;
+//         glfwTerminate();
+//         return -1;
+//     }
+//     glfwMakeContextCurrent(window);
+//     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+//
+//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//     {
+//         std::cout << "Failed to initialize GLAD" << std::endl;
+//         return -1;
+//     }
+//
+//     glViewport(0, 0, 1200, 600);
+//
+//     while (!glfwWindowShouldClose(window))
+//     {
+//         processInput(window);
+//
+//
+//         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+//         glClear(GL_COLOR_BUFFER_BIT);
+//
+//         glfwPollEvents();
+//         glfwSwapBuffers(window);
+//     }
+//
+//     glfwTerminate();
+//     return 0;
+// }
+//
+// I want to believe that what is behind this code is a try to draw a triangle
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
- 
+
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+int main()
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
- 
-int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-    };  
-
-    unsigned int VBO;  //Vertex Buffer Object
-    glGenBuffers(1, &VBO); 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
- 
-    GLFWwindow* window = glfwCreateWindow(1200, 600, "WINDOW NAME", NULL, NULL);
-    if (window == NULL) 
+    GLFWwindow* window = glfwCreateWindow(1200, 600, "this is window's name", NULL, NULL);
+    if (window == NULL)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        std::cout<<"Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::cout<<"Failed to initialize GLAD\n";
         return -1;
     }
 
-    glViewport(0, 0, 1200, 600);
-
-    while (!glfwWindowShouldClose(window)) 
+    while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
+        glfwPollEvents();
 
- 
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
- 
-        glfwPollEvents();
+
         glfwSwapBuffers(window);
     }
- 
+
     glfwTerminate();
     return 0;
 }
